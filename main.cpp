b03d6a70f31cfca93998323dc21506ffeec63cbf
voidrealms <bcairns@voidrealms.com>
2020-08-25 13:35:17 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <QCoreApplication>
+
+/*
+
+  What
+  Template design pattern
+
+  Description
+  The Template Method defines a skeleton of an algorithm in an operation, and defers some steps to subclasses
+
+  Why
+  We want to define what a class does but not how it does everything
+
+  Example
+  Chef
+
+  Note I really don't like this, I tend to use interfaces, but Qt does use this in some classes
+  For example - QTCPServer
+
+ */
+
+#include "baker.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    Baker baker;
+    baker.makeDinner();
+
+    return a.exec();
+}
